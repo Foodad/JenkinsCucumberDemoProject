dee8124b644e8b324b7abf034762080511399bfd
PDC4-Training.PDC4 <PDC4-Training.PDC4@P4A-508SD6Z.dir.svc.accenture.com>
2018-08-02 10:25:57 +0530

    logintourclass

--- src/test/java/skeleton/LoginNewTourStepDef.java
+++ src/test/java/skeleton/LoginNewTourStepDef.java
@@ -15,7 +15,7 @@ public class LoginNewTourStepDef {
 	private WebDriver driver;
 	
 	@Given("^user is on login page of herokuapp$")
-	public void user_is_on_login_page_of_newtours() throws Throwable {
+	public void user_is_on_login_page_of_herokuapp() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	   
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\pdc4-training.pdc4\\git\\devops2\\Resources\\chromedriver.exe");
